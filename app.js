badacba8f6215dea5981568afee8a7a0a84d420d
Felipe <fbf_felipeb@hotmail.com>
2022-08-07 17:06:56 -0300

    Delete ejs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('.src/routes/index');
@@ -11,12 +10,10 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'src','views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
