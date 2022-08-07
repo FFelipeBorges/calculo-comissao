791084028f7b860f1883f43ad0990756e37b97c6
Felipe <fbf_felipeb@hotmail.com>
2022-08-07 17:52:21 -0300

    Alter config.js to new sequelize

--- src/database/config/index.js
+++ src/database/config/index.js
@@ -1,10 +1,21 @@
+const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-module.exports = {
+const config = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_SCHEMA,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql'
-}
+};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+module.exports = sequelize;
