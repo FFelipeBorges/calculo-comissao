825d242eb4c03e303ba84ebb12ad86d0f9938343
Felipe <fbf_felipeb@hotmail.com>
2022-08-13 11:58:55 -0300

    eslintrc

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const transactionRouter = express.Router();
 
 transactionRouter.get('/', function (request, response) {
-  return response.json({ ok: 'ok' });
+    return response.json({ ok: 'ok' });
 });
 
 module.exports = transactionRouter;
