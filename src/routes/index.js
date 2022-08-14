4f09311964d2b40134a9183d1c9642dfb714c48c
Felipe <fbf_felipeb@hotmail.com>
2022-08-14 17:42:06 -0300

    fix app.js and routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const transactionsRouter = require('./transactions.routes');
+const comissionsRouter = require('./comissions.routes');
 
 router.use('/transactions', transactionsRouter);
-router.use('/comission', transactionsRouter);
+router.use('/comission', comissionsRouter);
 
 module.exports = router;
